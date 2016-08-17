7a8052a7dfa5df1261a6bc652f064088c7e52355
luisanguita <l.anguita.fernandez@gmail.com>
2016-08-16 23:39:23 -0300

    CHG: Se agrega menu lateral

---
+++ js/sv.js
@@ -0,0 +1,8 @@
+$( document ).ready(function(){
+    $("#menulat").click(function(){
+       $("#sidebar-wrapper").show();
+    });
+    $("#logolat").click(function(){
+        $("#sidebar-wrapper").hide();
+    });
+});
